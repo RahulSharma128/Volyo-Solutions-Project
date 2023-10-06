86060b315d6886f70026bd3fba4893563a022a52
Rahul Sharma <rockstarrahulsharma213@gmail.com>
2023-10-06 11:46:57 +0530

    only backend

---
+++ app/api/route.js
@@ -0,0 +1,158 @@
+import query from "./db.js";
+import { verify } from 'jsonwebtoken';
+
+
+function JWTauthentication(request) {
+  const authorizationHeader = request.headers.get('authorization');
+  if (!authorizationHeader) {
+    console.error('Authorization header is missing');
+    return new Response('Unauthorized', { status: 401 });
+  }
+
+  const tokenParts = authorizationHeader.split(' ');
+  if (tokenParts.length !== 2 || tokenParts[0].toLowerCase() !== 'bearer') {
+    console.error('Invalid authorization header');
+    return new Response('Unauthorized', { status: 401 });
+  }
+
+  const token = tokenParts[1];
+  const base64EncodedSecret = process.env.secretKey; 
+  const secretKey = Buffer.from(base64EncodedSecret, 'base64').toString('utf-8');
+
+  try {
+    const decoded = verify(token, secretKey, { algorithms: ['HS256'] });
+    console.log(decoded);
+
+    return null; 
+  } catch (error) {
+    console.error('Token verification failed:', error.message);
+    return new Response('Unauthorized', { status: 401 });
+  }
+}
+
+export async function GET(request) {
+  const authenticationResult = JWTauthentication(request);
+  if (authenticationResult !== null) {
+    return authenticationResult;
+  }
+    let tasks = await query({
+      query: "SELECT * FROM tasks"
+    });
+
+
+  const data = JSON.stringify(tasks);
+  return new Response(data, {
+    status: 200,
+  });
+}
+
+
+export async function POST(request) {
+
+  const authenticationResult = JWTauthentication(request);
+  if (authenticationResult !== null) {
+    return authenticationResult;
+  }
+
+    try {
+        const newTodo = await request.json(); 
+        const { id, title } = newTodo; 
+        const updateTasks = await query({
+            query: "INSERT INTO tasks (id, title) VALUES (?, ?)",
+            values: [id, title],
+        });
+
+        const result = updateTasks.affectedRows;
+        let message = "";
+        if (result) {
+            message = "Succesfully Added Task";
+        } else {
+            message = "error";
+        }
+
+        const product = {
+            id:id,
+            title: title
+        };
+
+        return new Response(JSON.stringify({
+            message: message,
+            status: 201,
+            product: product
+        }));
+    } catch (error) {
+        return new Response(JSON.stringify({
+            message:"Please check The Id",
+            status: 500,
+            data: request
+        }));
+    }
+}
+
+export async function DELETE(request) {
+  const authenticationResult = JWTauthentication(request);
+  if (authenticationResult !== null) {
+    return authenticationResult;
+  }
+
+    const url = new URL(request.url);
+    const taskId = url.searchParams.get('taskId');
+    try {
+        const deleteUser = await query({
+            query: "DELETE FROM tasks WHERE id = ?",
+            values: [taskId],
+        });
+        const result = deleteUser.affectedRows;
+        let message = "";
+        if (result) {
+            message = "success";
+        } else {
+            message = "error";
+        }
+        const product = {
+            id: taskId,
+        };
+        return new Response(JSON.stringify({
+            message: message,
+            status: 200,
+            product: product
+        }));
+    } catch (error) {
+        return new Response(JSON.stringify({
+            status: 500,
+            data: res
+        }));
+    }
+}
+
+export async function PUT(request) {
+  const authenticationResult = JWTauthentication(request);
+  if (authenticationResult !== null) {
+    return authenticationResult;
+  }
+    const url = new URL(request.url);
+    const taskId = url.searchParams.get('taskId');  
+    try {
+      const updateProducts = await query({
+        query: "UPDATE tasks SET completed = 1 WHERE id = ?",
+        values: [taskId],
+      });
+      const result = updateProducts.affectedRows;
+      let message = "";
+      if (result) {
+        message = "success";
+      } else {
+        message = "error";
+      }
+      return new Response(JSON.stringify({
+        message: message,
+        status: 200,
+      }));
+    } catch (error) {
+      return new Response(JSON.stringify({
+        status: 500,
+        data: error
+      }));
+    }
+  }
+  
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env : {
+        MYSQL_HOST: '127.0.0.1',
+        MYSQL_PORT: 3306,
+        MYSQL_DATABASE: 'todo-tasks',
+        MYSQL_USER: 'root',
+        MYSQL_PASSWORD: '',
+        secretKey:"Sl9Fajl6UjdrX1dfTTZ5UXdYdFYyX1Q="
+    }
+}
 
 module.exports = nextConfig
