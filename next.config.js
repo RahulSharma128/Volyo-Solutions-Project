a68efe3ca06048a80ca0c0cbc2bfad7bc043cfb5
Rahul Sharma <rockstarrahulsharma213@gmail.com>
2023-10-06 11:23:47 +0530

    JWT BRANCH AUTH

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env : {
-        MYSQL_HOST: '127.0.0.1',
-        MYSQL_PORT: 3306,
-        MYSQL_DATABASE: 'todo-tasks',
-        MYSQL_USER: 'root',
-        MYSQL_PASSWORD: '',
-        API_KEY:"Ej9zR7kM6yQwXtV2"
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
